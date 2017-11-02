1b4436dcd4b88d96a296ba5e47b4efba6b24a7d9
vitoriopaulo <vitoriopaulo@gmail.com>
2017-11-01 20:11:46 -0400

    Commit.
    Forms for add movie and update movie updated. Fields sinopse, actors
    and comment now have the texts prompted inside textarea elements.
    
    Get element function pending resolution. On the UI though, it was added
    a div analog to the get all movies above: it makes sense when thinking
    about visual design composition matters.

--- assets/scripts/movies/ui.js
+++ assets/scripts/movies/ui.js
@@ -13,7 +13,7 @@ const getAllMoviesSuccess = function (data) {
     movie.year + ' .' + '</p>' + '<br>' + '<p>' + 'Directed by: ' + movie.director + ' .' + '</p>' +
     '<br>' + '<p>' + 'Main Actors: ' + movie.actors + ' .' + '</p>' + '<br>' + '<p>' + 'Category: ' +
     movie.category + '</p>' + '<br>' + '<p>' + 'Status: ' + movie.status + ' .' + '<br>' + '<p>' +
-    'Comments: ' + movie.comment + '</p>' + '<br>' + '<p>' + 'Trailer url: ' + movie.trailer +
+    'Comments: ' + movie.comment + '</p>' + '<br>' + '<p>' + 'Trailer url: ' + movie.trailer + '</a>'
     '</p>' + '</li>'
   })
   $('#movies').html(newHTML)
@@ -42,7 +42,7 @@ console.log('Discovering what is it called data: ' + data)
   'Comments: ' + movie.comment + '</p>' + '<br>' + '<p>' + 'Trailer url: ' + movie.trailer +
   '</p>' + '</li>')
 
-  $('.my-input').val("")
+  $('.my-input').val('')
   // console.log(data)
   // store.movie = data.movie
   // console.log(store)
@@ -51,45 +51,49 @@ console.log('Discovering what is it called data: ' + data)
 
 const getOneMovieFailure = function (error) {
   console.error(error)
-  $('.my-input').val("")
+  $('.my-input').val('')
   $('#message_1').show().text('Error on getting movie. Try again.').fadeOut(5000)
 }
 
 const addMovieSuccess = function (data) {
   // console.log(data)
   // console.log('Password sucessfully changed!')
-  $('.my-input').val("")
+  $('.my-input').val('')
+  $('.my-txtarea').val('')
   $('#message_2').show().text('Movie sucessfully added.').fadeOut(5000)
 }
 
 const addMovieFailure = function (error) {
   console.log(error)
   // console.log('Password already token. Try again.')
-  $('.my-input').val("")
+  $('.my-input').val('')
+  $('.my-txtarea').val('')
   $('#message_2').show().text('Failure on trying to add movie to database. Try again.').fadeOut(5000)
 }
 
 const updateMovieSuccess = function (data) {
   // console.log(data)
-  $('.my-input').val("")
+  $('.my-input').val('')
+  $('.my-txtarea').val('')
   $('#message_3').show().text('Movie updated succesfully.').fadeOut(5000)
 }
 
 const updateMovieFailure = function (data) {
   // console.log(data)
-  $('.my-input').val("")
+  $('.my-input').val('')
+  $('.my-txtarea').val('')
   $('#message_3').show().text('Failure on updating movie.').fadeOut(5000)
 }
 
 const deleteMovieSuccess = function (data) {
   // console.log(data.song)
-  $('.my-input').val("")
+  $('.my-input').val('')
   // console.log('Signed out succesfully!')
   $('#message_4').show().text('Movie successfully deleted.').fadeOut(5000)
 }
 
 const deleteMovieFailure = function (error) {
-  $('.my-input').val("")
+  $('.my-input').val('')
   console.log(error)
   // console.log('Failure signing out.')
   $('#message_4').show().text('Failure deleting movie.').fadeOut(5000)
